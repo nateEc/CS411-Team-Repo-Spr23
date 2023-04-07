123e9c9b0970d15aab43524eb40d794edaf90f52
cjpar <104914200+cjpar@users.noreply.github.com>
2023-04-06 21:24:02 -0400

    prototype with frontend and almost functioning api call

--- project/routes/index.js
+++ project/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const app = require("../app");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
+
+router.get('/', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  const data = {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website!'
+  };
+  res.render('index', data);
+});
 module.exports = router;
