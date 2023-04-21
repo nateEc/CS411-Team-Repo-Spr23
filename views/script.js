ec3f76dfb22ad4675255a8a13ab60547a000185a
cjpar <104914200+cjpar@users.noreply.github.com>
2023-04-21 12:26:17 -0400

    search movie output simply incredible now

--- views/script.js
+++ views/script.js
@@ -1,5 +1,13 @@
- // move this function elsewhere
-    function createCardElement(title, posterUrl, rating, overview) {
+const API_KEY = "api_key=b9ab825c6c7df22351f2927746188d59"
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const SEARCH_URL = BASE_URL + '/search/movie?'+API_KEY;
+
+const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('search');
+function createCardElement(title, posterUrl, rating, overview) {
     const card = document.createElement("div");
     card.className = "card";
 
@@ -25,43 +33,64 @@
 
     return card;
 }
+function searchMovies(url) {
+    const resultsList = document.querySelector(".result");
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            /*data.results.slice(0, 8).forEach((result) => {
+                const card = createCardElement(
+                    result.title,
+                    `https://image.tmdb.org/t/p/w200${result.poster_path}`,
+                    result.vote_average,
+                    result.overview
+                );
+                resultsList.appendChild(card);
+            });*/
+            main.innerHTML = '';
+            data.results.slice(0, 8).forEach((result) => {
+                const {title, poster_path, vote_average, overview, id} = result;
+                const resultsList = document.createElement('div');
+                resultsList.classList.add('movie');
+                resultsList.innerHTML = `
+             <img src="${poster_path ? IMG_URL + poster_path : "http://via.placeholder.com/1080x1580"}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+                <br/> 
+                <button class="know-more" id="${id}">Know More</button
+            </div>
+        `
+                main.appendChild(resultsList);
+                document.getElementById(id).addEventListener('click', () => {
+                    console.log(id)
+                })
+            })
+        })
+        .catch((error) => console.error(error));
+}
+
+function getColor(vote) {
+    if(vote>= 8){
+        return 'green'
+    }else if(vote >= 5){
+        return "orange"
+    }else{
+        return 'red'
+    }
+}
 
     let searchButton = document.querySelector("input[type='submit']");
     searchButton.addEventListener("click", (event) => { //searchButton.addEventListener("submit", async (event) => {
     event.preventDefault();
     let searchInput = document.querySelector("#usrinput");
     const query = searchInput.value;
-    const SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=b9ab825c6c7df22351f2927746188d59&query=$%7Bquery%7D"
-      const API_KEY = "b9ab825c6c7df22351f2927746188d59"
-    const resultsList = document.querySelector(".result");
-    console.log(API_KEY)
-    console.log(SEARCH_URL)
-    const url = `${SEARCH_URL}?api_key=${API_KEY}&query=${query}`;
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-    resultsList.innerHTML = "";
-
-    data.results.slice(0, 8).forEach((result) => {
-    const card = createCardElement(
-    result.title,
-    `https://image.tmdb.org/t/p/w200${result.poster_path}`,
-    result.vote_average,
-    result.overview
-    );
-    resultsList.appendChild(card);
-});
-    /*resultsList.style.display = "flex";
-    resultsList.style.flexDirection = "row";
-    resultsList.style.listStyle = "none";
-    resultsList.style.margin = "0";
-    resultsList.style.padding = "0";
-    const liItems = document.querySelectorAll(".result li");
-    liItems.forEach((li) => {
-        li.style.width = "220px";
-        li.style.height = "350px";
-        li.style.marginRight = "20px";
-    })*/
-})
-    .catch((error) => console.error(error));
+    console.log(API_KEY);
+    console.log(SEARCH_URL);
+    const url = SEARCH_URL+'&query='+query;
+    searchMovies(url);
 });
