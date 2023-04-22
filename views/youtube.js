a0184d7e144440d88f8c91fc475f43db76b6a29c
Deepp805 <deeppatel1944@gmail.com>
2023-04-21 23:30:54 -0400

    added functionality for 2nd api to redirect to the youtube trailer page for a particular movie

---
+++ views/youtube.js
@@ -0,0 +1,38 @@
+const API_KEY = 'AIzaSyAea3b5J5hWm4nNH5HdJIc0CEylRUKEt_M';
+
+// Search for a video trailer based on a movie name
+async function searchTrailer(movieName) {
+  console.log("the function is called");
+  try {
+    // Encode the movie name for use in the API request
+    const encodedMovieName = encodeURIComponent(`${movieName} trailer`);
+    
+    // Construct the API request URL
+    const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${encodedMovieName}&type=video&part=snippet&maxResults=1`;
+    
+    // Make the API request
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    // Extract the video ID from the search results
+    const videoId = data.items[0].id.videoId;
+    
+    // Construct the URL for the trailer video
+    const trailerUrl = `https://www.youtube.com/watch?v=${videoId}`;
+    
+    return trailerUrl;
+  } catch (error) {
+    console.error('An error occurred here: ', error);
+    const trailerUrl = 'https://www.youtube.com/';
+    return trailerUrl;
+  }
+}
+
+export async function getTrailerUrl(movieName) {
+  const trailerUrl = await searchTrailer(movieName);
+  // console.log(trailerUrl);
+  return trailerUrl;
+}
+
+
+console.log(getTrailerUrl('The Matrix'));
