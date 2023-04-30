3255362697721857a99a5e6b99c45b50d745eec3
cjpar <104914200+cjpar@users.noreply.github.com>
2023-04-30 14:20:07 -0400

    formatting of api key var

--- views/scripts/youtube.js
+++ views/scripts/youtube.js
@@ -1,5 +1,5 @@
 import {YOUTUBE_API} from "../../config/config.js";
-const YOUTUBE_API_KEY = "api_key="+YOUTUBE_API;
+const YOUTUBE_API_KEY = YOUTUBE_API;
 
 // Search for a video trailer based on a movie name
 async function searchTrailer(movieName) {
